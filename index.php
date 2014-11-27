18b9e1836983ab9797655ac58ed25a8cd44e32da
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2014-11-27 12:27:25 +0100

    devel web interface

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+    <meta name="author" content="Kévin PERROTTA, Paul FRIDRICK, Léonard PERRIER et Arnaud DEWULF"/>
+    <meta name="description" content="Découvrez le tchat de recrutement de Télécom Siant-Étienne."/>
+    <meta name="keywords" content="Télécom Saint-Étienne, TSE, tchat, chat, recrutement" />
+
+    <title>Tchat TSE</title>
+    <link rel="shortcut icon" href="images/logo-TSE.png">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/customize.css" rel="stylesheet">
+</head>
+
+<body>
+    <img id="imgHeader" src="images/header-TSE.jpg" alt="header">
+    <h2 class="mainTitle">Bienvenue sur le Tchat de Télécom Saint-Étienne</h2>
+    
+    <div class="row">
+        <div class="col-md-3">
+            <h2>NBTC</h2>
+        </div>
+        <div class="col-md-9">
+            <h2>auie,c</h2>
+        </div>
+    </div>
+</body>
