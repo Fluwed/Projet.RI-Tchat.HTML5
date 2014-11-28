3a1d9cb05661c08494128ac1c169c0faa02652fd
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2014-11-28 10:14:30 +0100

    devel db acces

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
+<?php/*
+    $link = mysqli_connect("localhost", "paul", "paul", "tchat_recrutement");
+    mysqli_set_charset($link , "utf8");
+    
+    if (mysqli_connect_errno()) {
+        printf("Échec de la connexion SQL : %s\n", mysqli_connect_error());
+        exit();
+    }
+*/?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
     <meta name="author" content="Kévin PERROTTA, Paul FRIDRICK, Léonard PERRIER et Arnaud DEWULF"/>
-    <meta name="description" content="Découvrez le tchat de recrutement de Télécom Siant-Étienne."/>
-    <meta name="keywords" content="Télécom Saint-Étienne, TSE, tchat, chat, recrutement" />
+    <meta name="description" content="Découvrez le tchat de Télécom Siant-Étienne."/>
+    <meta name="keywords" content="Télécom Saint-Étienne, TSE, tchat, chat, recrutement"/>
 
     <title>Tchat TSE</title>
     <link rel="shortcut icon" href="images/logo-TSE.png">
@@ -14,30 +24,41 @@
 </head>
 
 <body>
-    <img id="imgHeader" src="images/header-TSE.jpg" alt="header">
+    <img class="imgHeader" src="images/header-TSE.jpg" alt="header">
     <h2 class="mainTitle">Tchat Télécom Saint-Étienne</h2>
-    
+
     <div class="row" id="padding">
+
         <div class="col-sm-2" id="center">
-			<div class="list-group">
-			<div id="gras">
-				<h3 class="list-group-item">Menu</h3>
-			</div>	
-				<a href="index.php" class="list-group-item active">Accueil</a>
-				<a href="agenda.html" class="list-group-item">Connexion</a>
-				<a href="abonnement_actu.html" class="list-group-item">Admin</a>
-			</div>
-		</div>
-		
+            <div class="list-group">
+                <h3 class="list-group-item">Menu</h3>
+                <a href="index.php" class="list-group-item active">Accueil</a>
+                <a href="" class="list-group-item">Connexion</a>
+                <a href="" class="list-group-item">Admin</a>
+            </div>
+        </div>
+
         <div class="col-sm-10"><br>
             <div class="panel panel-default">
-				<div class="panel-heading">
-				  <h2 class="panel-title">Salons de discussion</h2>
-				</div>
-				<div class="panel-body" id="">
-					<p> coucou</p>
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="panel-heading">
+                    <h2 class="panel-title">Salons de discussion</h2>
+                </div>
+                <div class="panel-body" id="">
+                    <p>coucou</p>
+
+                    <?php/*
+                        $query = "SELECT * FROM salons";
+                        if ($result = mysqli_query($link, $query)) {
+                            while($row = mysqli_fetch_assoc($result)) {
+                                printf ("%s (%s)\n", $row["id"], $row["nom"]);
+                            }
+                            mysqli_free_result($result);
+                        }
+                    */?>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
