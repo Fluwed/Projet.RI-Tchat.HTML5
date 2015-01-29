4faf876873c7dc7039352deaa8a92a7dbc815ba3
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 16:59:43 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -1,4 +1,24 @@
 <?php
+    if(isset($_POST['signup'])) {
+        $username=$_POST['username'];
+        $passwd=$_POST['passwd'];
+        $passwd2=$_POST['passwd2'];
+
+        if ($passwd != $passwd2) {
+            
+        }
+        else {
+            require("connectdb.php");
+            $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
+            echo $query;
+            $result = mysqli_query($link, $query) or die(mysqli_error($link));
+            mysqli_free_result($result) or die(mysqli_error($link));
+            mysqli_close($link) or die(mysqli_error($link));
+        }
+    }
+    else {
+        echo "An error occured.";
+    }
     header("Location: index.php");
     exit;
 ?>
