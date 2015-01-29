5ddefbbfbf5b6f953c1f3ce49aa319dd4686ce47
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 17:04:46 +0100

    test create user

--- connectdb.php
+++ connectdb.php
@@ -1,4 +1,4 @@
 <?php
-    $link = mysqli_connect("pc-paul:3306", "lamp", "lamp", "tchat_tse") or die(mysqli_error($link));
-    mysqli_set_charset($link , "utf8") or die(mysqli_error($link));
+    $link = mysqli_connect("pc-paul:3306", "lamp", "lamp", "tchat_tse") //or die(mysqli_error($link));
+    mysqli_set_charset($link , "utf8") //or die(mysqli_error($link));
 ?>
