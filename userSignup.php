2ee4fbf4b8e0e19b3047a832fc63bf149fe081a8
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 15:56:30 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -25,7 +25,12 @@
         
     }
     else {
-        require("connectdb.php");
+        $link = mysqli_connect("pc-paul:3306", "lamp", "lamp", "tchat_tse");
+    mysqli_set_charset($link , "utf8");
+
+    if (mysqli_connect_errno()) {
+        printf("Échec de la connexion SQL : %s\n", mysqli_connect_error());
+    }
         $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
         echo $query;
         $result = mysqli_query($link, $query) or die(printf("Échec de la connexion SQL : %s\n", mysqli_connect_error()););
