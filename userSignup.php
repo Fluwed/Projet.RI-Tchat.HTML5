503c09cca168a5aeeaa9a37d11944ccfefdbfea2
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 17:07:36 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -7,10 +7,10 @@
     else {
         require("connectdb.php");
         $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
-        $result = mysqli_query($link, $query) //or die(mysqli_error($link));
-        mysqli_free_result($result) //or die(mysqli_error($link));
-        mysqli_close($link) //or die(mysqli_error($link));
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        mysqli_free_result($result) or die(mysqli_error($link));
+        mysqli_close($link) or die(mysqli_error($link));
     }
-    header("Location: index.php");
+    header("Location: http://www.google.com");
     exit;
 ?>
