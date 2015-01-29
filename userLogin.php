be340b46b6aaf38909d7bacfed5d212a4fa67ea8
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 17:01:02 +0100

    test create user

--- userLogin.php
+++ userLogin.php
@@ -2,7 +2,6 @@
     $username=$_POST['username'];
     $password=$_POST['password'];
     $remember=$_POST['remember'];
-
     require("connectdb.php");
     $query = "SELECT * FROM Auteurs WHERE user='".$username."'";
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
--- userSignup.php
+++ userSignup.php
@@ -1,23 +1,15 @@
 <?php
-    if(isset($_POST['signup'])) {
-        $username=$_POST['username'];
-        $passwd=$_POST['passwd'];
-        $passwd2=$_POST['passwd2'];
-
-        if ($passwd != $passwd2) {
-            
-        }
-        else {
-            require("connectdb.php");
-            $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
-            echo $query;
-            $result = mysqli_query($link, $query) or die(mysqli_error($link));
-            mysqli_free_result($result) or die(mysqli_error($link));
-            mysqli_close($link) or die(mysqli_error($link));
-        }
+    $username=$_POST['username'];
+    $passwd=$_POST['passwd'];
+    $passwd2=$_POST['passwd2'];
+    if ($passwd != $passwd2) {
     }
     else {
-        echo "An error occured.";
+        require("connectdb.php");
+        $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        mysqli_free_result($result) or die(mysqli_error($link));
+        mysqli_close($link) or die(mysqli_error($link));
     }
     header("Location: index.php");
     exit;
