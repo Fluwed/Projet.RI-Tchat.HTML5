795cce032eb33473492a1cbc4a00e374f7b05468
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 15:52:27 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+        <meta name="author" content="Kévin PERROTTA, Paul FRIDRICK, Léonard PERRIER et Arnaud DEWULF"/>
+        <meta name="description" content="Découvrez le tchat de Télécom Siant-Étienne."/>
+        <meta name="keywords" content="Télécom Saint-Étienne, TSE, tchat, chat, recrutement"/>
+
+        <title>Tchat TSE</title>
+        <link rel="shortcut icon" href="images/minilogo-TSE.png">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/customize.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    </head>
+
+    <body>
+
 <?php
     $username=$_POST['username'];
     $passwd=$_POST['passwd'];
@@ -7,7 +25,7 @@
         
     }
     else {
-        include("connectdb.php");
+        require("connectdb.php");
         $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
         echo $query;
         $result = mysqli_query($link, $query) or die(printf("Échec de la connexion SQL : %s\n", mysqli_connect_error()););
@@ -15,3 +33,6 @@
         mysqli_close($link);
     }
 ?>
+
+    </body>
+</html>
