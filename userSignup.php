72f3a3fb0dc56acbcceb81cfd0f991f2e14c92e5
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 16:35:51 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
-        <meta name="author" content="Kévin PERROTTA, Paul FRIDRICK, Léonard PERRIER et Arnaud DEWULF"/>
-        <meta name="description" content="Découvrez le tchat de Télécom Siant-Étienne."/>
-        <meta name="keywords" content="Télécom Saint-Étienne, TSE, tchat, chat, recrutement"/>
 
-        <title>Tchat TSE</title>
-        <link rel="shortcut icon" href="images/minilogo-TSE.png">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/customize.css" rel="stylesheet">
-    </head>
-
-    <body>
         <?php
             $username=$_POST['username'];
             $passwd=$_POST['passwd'];
@@ -29,8 +14,7 @@
                 $result = mysqli_query($link, $query) or die(mysqli_error($link));
                 mysqli_free_result($result) or die(mysqli_error($link));
                 mysqli_close($link) or die(mysqli_error($link));
+                header('Location: /index.php');
             }
         ?>
-        <meta http-equiv="refresh" content="1;url=index.php" />
-    </body>
-</html>
+
