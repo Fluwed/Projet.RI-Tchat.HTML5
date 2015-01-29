a8cb1cb0d3f76487b059bd61497401782761219e
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 16:14:24 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -1,20 +1,18 @@
+<?php
+    $username=$_POST['username'];
+    $passwd=$_POST['passwd'];
+    $passwd2=$_POST['passwd2'];
 
-            <?php
-                $username=$_POST['username'];
-                $passwd=$_POST['passwd'];
-                $passwd2=$_POST['passwd2'];
-
-                if ($passwd != $passwd2) {
-                    
-                }
-                else {
-                    require("connectdb.php");
-                    $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
-                    echo $query;
-                    $result = mysqli_query($link, $query) or die(mysqli_error($link));
-                    mysqli_free_result($result) or die(mysqli_error($link));
-                    mysqli_close($link) or die(mysqli_error($link));
-                }
-                echo "<form action='index.php'>";
-            ?>
-
+    if ($passwd != $passwd2) {
+        
+    }
+    else {
+        require("connectdb.php");
+        $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
+        echo $query;
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        mysqli_free_result($result) or die(mysqli_error($link));
+        mysqli_close($link) or die(mysqli_error($link));
+    }
+    echo "<form action='index.php' method='post'>";
+?>
