2e37d0d2c1bd5448de7569fc2c5d9ed9a4aa4326
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-01-29 16:30:33 +0100

    test create user

--- userSignup.php
+++ userSignup.php
@@ -14,25 +14,23 @@
     </head>
 
     <body>
-        <div>
-            <?php
-                $username=$_POST['username'];
-                $passwd=$_POST['passwd'];
-                $passwd2=$_POST['passwd2'];
+        <?php
+            $username=$_POST['username'];
+            $passwd=$_POST['passwd'];
+            $passwd2=$_POST['passwd2'];
 
-                if ($passwd != $passwd2) {
-                    
-                }
-                else {
-                    require("connectdb.php");
-                    $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
-                    echo $query;
-                    $result = mysqli_query($link, $query) or die(mysqli_error($link));
-                    mysqli_free_result($result) or die(mysqli_error($link));
-                    mysqli_close($link) or die(mysqli_error($link));
-                }
-            ?>
-        </div>
+            if ($passwd != $passwd2) {
+                
+            }
+            else {
+                require("connectdb.php");
+                $query = "INSERT INTO Auteurs (user, password) VALUES ('".$username."','".$passwd."')";
+                //echo $query;
+                $result = mysqli_query($link, $query) or die(mysqli_error($link));
+                mysqli_free_result($result) or die(mysqli_error($link));
+                mysqli_close($link) or die(mysqli_error($link));
+            }
+        ?>
         <meta http-equiv="refresh" content="1;url=index.php" />
     </body>
 </html>
